8c9dc02919e144fb8348e97caf30a77bbcb6bbde
mustafa batuhan bayoglu <bayoglubatuhan@gmail.com>
2021-04-03 17:48:52 +0300

    Update Power source file

--- Recursive Algorithms/Sources/Power.cpp
+++ Recursive Algorithms/Sources/Power.cpp
@@ -4,10 +4,12 @@
 using namespace std;
 
 double Power1(double, int);
+double Power2(double, int);
 
 int main() {
 
 	cout << "Power1: 3^4 =  " << Power1(3, 4) << endl;
+	cout << "Power1: 3^4 =  " << Power2(3, 3) << endl;
 	return 0;
 
 } // end-main
@@ -15,7 +17,7 @@ int main() {
 // Computes a^n
 double Power1(double number, int power) {
 
-	//Base case
+	// Base case
 	if (power == 0) return 1;
 	if (power == 1) return number;
 
@@ -26,3 +28,20 @@ double Power1(double number, int power) {
 	return partialResult * number;
 
 } // end-Power1
+
+// Computes a^n
+double Power2(double number, int power){
+
+	// Base case
+	if (power == 0) return 1;
+	if (power == 1) return number;
+
+	// Divide and conquer
+	double partialResult = Power2(number, power / 2);
+
+	// Merge
+	double result = partialResult * partialResult;
+	if (power % 2 == 1) result *= number; // if power is odd, result = result * number
+	return result;
+
+}// end-Power2
