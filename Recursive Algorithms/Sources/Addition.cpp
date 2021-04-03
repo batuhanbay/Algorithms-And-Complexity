05b0f006aae4bc9d12495c47a7b5705cc7b80878
mustafa batuhan bayoglu <bayoglubatuhan@gmail.com>
2021-04-03 17:04:18 +0300

    Update Addition source file

--- Recursive Algorithms/Sources/Addition.cpp
+++ Recursive Algorithms/Sources/Addition.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int Sum1(int);
 int Sum2(vector<int>&, int);
+int Sum3(vector<int>&, int, int);
 int main() {
 
 	int result = Sum1(4);
 	cout << "Sum1: " << Sum1(4) << endl;
 	vector<int> A = { 3, 1, 6, 5, 8, 2, 4, 7 }; // sum = 36
 	cout << "Sum2: " << Sum2(A, A.size()) << endl;
+	cout << "Sum3: " << Sum3(A, 0, A.size() - 1) << endl;
 	return 0;
 
 } // end-main
@@ -31,7 +33,7 @@ int Sum1(int n) {
 int Sum2(vector<int>& A, int size) {
 	// Base case
 	if (size == 0) return 0;
-	if (size == 1) return 1;
+	if (size == 1) return A[0];
 
 	// Divide and conquer
 	int localSum = Sum2(A, size - 1);
@@ -39,3 +41,17 @@ int Sum2(vector<int>& A, int size) {
 	// Merge
 	return localSum + A[size - 1];
 } // end-Sum2
+
+// Computes 1 + 2 + 3 + ... + n
+int Sum3(vector<int>& A, int left, int right) {
+	// Base case
+	if (left > right) return 0;
+	if (left == right) return A[left];
+
+	// Divide and conquer
+	int middle = (left + right) / 2;
+	int localSum1 = Sum3(A, left, middle);
+	int localSum2 = Sum3(A, middle + 1, right);
+	// Merge
+	return localSum1 + localSum2;
+} // end-Sum3
